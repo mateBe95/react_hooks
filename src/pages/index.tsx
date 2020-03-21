2e463f30d5db571317b93a8030730463030957ac
Patryk Obiedziński <paobiedzinski@gmail.com>
2020-03-21 22:35:49 +0100

    Tsconfig rules extended

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import SideNav from "../components/SideNav/SideNav";
-import { Layout } from "antd";
+import {Layout} from "antd";
 import 'antd/dist/antd.css';
 
-
-function useStats() {
-  const [stats, setStats] = useState();
-  useEffect(() => {
-    async function fetchData() {
-      const data = await fetch("https://covid19.mathdro.id/api").then(res =>
-        res.json()
-      );
-      setStats(data);
-    }
-    fetchData();
-  }, []);
-  return stats;
-}
-
-function Stats() {
-  const stats = useStats();
-  console.log(stats);
-  return (
-    <div>
-      <p>Stats: </p>
-    </div>
-  );
-}
-
 export default function IndexPage() {
-  return (
-      <Layout>
-        <SideNav/>
-      </Layout>
-  );
+    return (
+        <Layout>
+            <SideNav/>
+        </Layout>
+    );
 }
