6500846392ef00d942c092ac0c0d7f4ff49b762f
Mateusz <mateusz.bieda95@gmail.com>
2020-03-22 16:09:27 +0100

    disable deps rule

--- src/components/FirstContent/hook.tsx
+++ src/components/FirstContent/hook.tsx
@@ -1,37 +1,38 @@
-import {useEffect, useState} from "react";
+import { useState, useEffect } from "react";
 
 interface CoronaValue {
-    value: number;
-    detail: string;
+  value: number;
+  detail: string;
 }
 
 interface CoronaData {
-    confirmed: CoronaValue;
-    recovered: CoronaValue;
-    deaths: CoronaValue;
+  confirmed: CoronaValue;
+  recovered: CoronaValue;
+  deaths: CoronaValue;
 }
 
-type FetchReturn = 
-| {data: CoronaData, loading: false} 
-| {data: undefined, loading: true};
+type FetchReturn =
+  | { data: CoronaData; loading: false }
+  | { data: undefined; loading: true };
 
 function useFetch(url: string) {
-    const [data, setData] = useState<CoronaData | undefined>(undefined);
-    const [loading, setLoading] = useState(true);
-   
-      async function fetchUrl() {
-        const response = await fetch(url);
-        const json = await response.json();
+  const [data, setData] = useState<CoronaData | undefined>(undefined);
+  const [loading, setLoading] = useState(true);
 
-        setData(json);
-        setLoading(false)
-      }
+  async function fetchUrl() {
+    const response = await fetch(url);
+    const json = await response.json();
 
-      useEffect(() => {
-        fetchUrl();
-    }, []);
-  
-    return {data, loading} as FetchReturn;
+    setData(json);
+    setLoading(false);
   }
-  
-  export { useFetch }
+
+  useEffect(() => {
+    fetchUrl();
+// eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { data, loading } as FetchReturn;
+}
+
+export { useFetch };
