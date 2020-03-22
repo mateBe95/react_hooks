d07b3572d0248e62c98249a66b00a34c6885e074
Patryk Obiedziński <paobiedzinski@gmail.com>
2020-03-22 15:02:17 +0100

    BrowserRouter added

--- src/components/AppContent/AppContent.tsx
+++ src/components/AppContent/AppContent.tsx
@@ -1,34 +1,32 @@
 import React from "react";
-import { Layout } from "antd";
+import {Layout} from "antd";
 import './AppContent.scss'
-import { routes } from "../../routing";
-import {
-  Route,
-  Switch
-} from "react-router-dom";
-const { Content } = Layout;
+import {routes} from "../../routing";
+import {Route, Switch} from "react-router-dom";
+
+const {Content} = Layout;
 export default function AppContent() {
-  return (
-    <>
-      <Content
-        className="site-layout-background"
-        style={{
-          margin: "24px 16px",
-          padding: 24,
-          minHeight: "100vh"
-        }}
-      >
-        <Switch>
-            {routes.map((route: any, index: number) => (
-              <Route
-                key={index}
-                path={route.path}
-                exact={route.exact}
-                component={route.component}
-              />
-            ))}
-          </Switch>
-      </Content>
-    </>
-  );
+    return (
+        <>
+            <Content
+                className="site-layout-background"
+                style={{
+                    margin: "24px 16px",
+                    padding: 24,
+                    minHeight: "100vh"
+                }}
+            >
+                <Switch>
+                    {routes.map((route: any, index: number) => (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            exact={route.exact}
+                            component={route.component}
+                        />
+                    ))}
+                </Switch>
+            </Content>
+        </>
+    );
 }
