a268ed9dad684d44491b0f02460222109d95526e
Patryk Obiedziński <paobiedzinski@gmail.com>
2020-03-22 16:43:14 +0100

    Hook in the FirstContent fixed

--- src/components/FirstContent/FirstContent.tsx
+++ src/components/FirstContent/FirstContent.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import CoronaCard from "../Card/CoronaCard";
 import "./FirstContent.scss";
-import { Row } from 'antd';
-import { useFetch } from "./hook";
-
+import {Row} from 'antd';
+import {useFetch} from "./hook";
 
 
 export default function FirstContent() {
-  const {data, loading} = useFetch('https://covid19.mathdro.id/api/countries/POLAND/')
+    const {data, loading} = useFetch('https://covid19.mathdro.id/api/countries/POLAND/');
 
-  console.log(data)
-  if (loading) return <p>Loading...</p>
-  return (
-    <div className="site-card-wrapper">
-     <Row gutter={16}>
+    console.log(data)
+    if (loading) return <p>Loading...</p>
+    return (
+        <div className="site-card-wrapper">
+            <Row gutter={16}>
      <CoronaCard
         title={"Obecnie zarażonych"}
         content={data?.confirmed.value}
--- src/components/FirstContent/hook.tsx
+++ src/components/FirstContent/hook.tsx
@@ -1,38 +1,37 @@
-import { useState, useEffect } from "react";
+import {useEffect, useState} from "react";
 
 interface CoronaValue {
-  value: number;
-  detail: string;
+    value: number;
+    detail: string;
 }
 
 interface CoronaData {
-  confirmed: CoronaValue;
-  recovered: CoronaValue;
-  deaths: CoronaValue;
+    confirmed: CoronaValue;
+    recovered: CoronaValue;
+    deaths: CoronaValue;
 }
 
 type FetchReturn =
-  | { data: CoronaData; loading: false }
-  | { data: undefined; loading: true };
+    | { data: CoronaData, loading: false }
+    | { data: undefined, loading: true };
 
 function useFetch(url: string) {
-  const [data, setData] = useState<CoronaData | undefined>(undefined);
-  const [loading, setLoading] = useState(true);
+    const [data, setData] = useState<CoronaData | undefined>(undefined);
+    const [loading, setLoading] = useState(true);
 
-  async function fetchUrl() {
-    const response = await fetch(url);
-    const json = await response.json();
+    useEffect(() => {
+        async function getFetchUrl() {
+            const response = await fetch(url);
+            const json = await response.json();
 
-    setData(json);
-    setLoading(false);
-  }
+            setData(json);
+            setLoading(false);
+        }
 
-  useEffect(() => {
-    fetchUrl();
-// eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+        getFetchUrl();
+    }, [url]);
 
-  return { data, loading } as FetchReturn;
+    return {data, loading} as FetchReturn;
 }
 
-export { useFetch };
+export {useFetch}
